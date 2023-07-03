86ed28979d179dd978a4de59546e492fd9e5e58f
Splinter-NGoH <ahmedmansy265@gmail.com>
2023-07-03 03:00:09 +0300

    settings

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -83,13 +83,22 @@ AUTH_USER_MODEL = "accounts.Account"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "hydra",
+        "USER": "masny",
+        "PASSWORD": "4321actioon",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
