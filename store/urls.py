b1fb1fc9d93441dbb1a32816960bc4b9043e9458
Splinter-NGoH <ahmedmansy265@gmail.com>
2023-07-03 04:18:51 +0300

    store

--- store/urls.py
+++ store/urls.py
@@ -8,7 +8,7 @@ router = routers.DefaultRouter()
 router.register(r"machinemodel", views.MyModelViewSet)
 urlpatterns = [
     path("", include(router.urls)),
-    path("", views.store, name="store"),
+    path("products/", views.store, name="store"),
     path("category/<slug:category_slug>/", views.store, name="products_by_category"),
     path(
         "category/<slug:category_slug>/<slug:product_slug>/",
