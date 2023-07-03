8d8e149bab6a7169fb845453c6a78634d2d220be
Splinter-NGoH <ahmedmansy265@gmail.com>
2023-07-03 03:02:27 +0300

    settings

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "hydra",
-        "USER": "masny",
+        "USER": "mansy",
         "PASSWORD": "4321actioon",
         "HOST": "localhost",
         "PORT": "",
